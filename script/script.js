aee936b19bf15709c537735b14c578b9d6f9dbba
Evan C. Arias <132534598+evan-arias@users.noreply.github.com>
2024-12-16 11:16:59 -0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'  
+        });
+    });
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
